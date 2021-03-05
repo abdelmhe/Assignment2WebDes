6343cd175d9def614d661101e74bef93adfffdeb
abdelmhe <abdelmhe@sheridancollege.ca>
2021-03-05 05:46:23 +0200

    Creating exception class

--- src/main/java/com/abdelmhe/sheridancollege/demo/exception/RecordNotFoundException.java
+++ src/main/java/com/abdelmhe/sheridancollege/demo/exception/RecordNotFoundException.java
@@ -1,4 +1,17 @@
 package com.abdelmhe.sheridancollege.demo.exception;
 
-public class RecordNotFoundException {
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class RecordNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
+
+    public RecordNotFoundException(String message, Throwable t) {
+        super(message, t);
+    }
+
 }
+
