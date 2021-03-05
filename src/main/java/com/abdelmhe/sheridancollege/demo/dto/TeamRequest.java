9797413d0f1f2ade8204feca347a77ba8ac71eaa
abdelmhe <abdelmhe@sheridancollege.ca>
2021-03-05 05:31:16 +0200

    Creating team request class

--- src/main/java/com/abdelmhe/sheridancollege/demo/dto/TeamRequest.java
+++ src/main/java/com/abdelmhe/sheridancollege/demo/dto/TeamRequest.java
@@ -1,4 +1,19 @@
 package com.abdelmhe.sheridancollege.demo.dto;
+import lombok.Data;
 
+@Data
 public class TeamRequest {
+
+    private String teamName;
+
+    private String continent;
+
+    private String played;
+
+    private String won;
+
+    private String drawn;
+
+    private String lost;
+
 }
