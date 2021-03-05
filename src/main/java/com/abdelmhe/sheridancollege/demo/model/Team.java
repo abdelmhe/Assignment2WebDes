df467076e2f0d9c2046630e40076087bbc52e4ea
abdelmhe <abdelmhe@sheridancollege.ca>
2021-03-05 05:47:22 +0200

    Creating team model class

--- src/main/java/com/abdelmhe/sheridancollege/demo/model/Team.java
+++ src/main/java/com/abdelmhe/sheridancollege/demo/model/Team.java
@@ -1,4 +1,99 @@
 package com.abdelmhe.sheridancollege.demo.model;
 
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="teams")
 public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="team_id")
+    private Long teamID;
+
+    @Column(name="team_name")
+    private String teamName;
+
+    @Column(name="team_flag")
+    private String teamFlag;
+
+    @Column(name="continent")
+    private String continent;
+
+    @Column(name="played")
+    private Integer played;
+
+    @Column(name="won")
+    private Integer won;
+
+    @Column(name="drawn")
+    private Integer drawn;
+
+    @Column(name="lost")
+    private Integer lost;
+
+    public Long getTeamID() {
+        return teamID;
+    }
+
+    public void setTeamID(Long teamID) {
+        this.teamID = teamID;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public Integer getPlayed() {
+        return played;
+    }
+
+    public void setPlayed(Integer played) {
+        this.played = played;
+    }
+
+    public Integer getWon() {
+        return won;
+    }
+
+    public void setWon(Integer won) {
+        this.won = won;
+    }
+
+    public Integer getDrawn() {
+        return drawn;
+    }
+
+    public void setDrawn(Integer drawn) {
+        this.drawn = drawn;
+    }
+
+    public Integer getLost() {
+        return lost;
+    }
+
+    public void setLost(Integer lost) {
+        this.lost = lost;
+    }
+
+    public String getTeamFlag() {
+        return teamFlag;
+    }
+
+    public void setTeamFlag(String teamFlag) {
+        this.teamFlag = teamFlag;
+    }
 }
