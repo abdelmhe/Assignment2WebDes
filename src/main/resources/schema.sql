f2a50fc5a5eb892a72623efcc54a53a545387fa5
abdelmhe <abdelmhe@sheridancollege.ca>
2021-03-05 05:58:05 +0200

    creating team structure using table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,12 @@
+DROP TABLE IF EXISTS TEAMS;
 
+CREATE TABLE `TEAMS` (
+                         team_id INT AUTO_INCREMENT PRIMARY KEY,
+                         team_name VARCHAR(250) NOT NULL,
+                         team_flag VARCHAR(500) NOT NULL,
+                         continent VARCHAR(250) NOT NULL,
+                         played INT(50) DEFAULT NULL,
+                         won INT(50) DEFAULT NULL,
+                         drawn INT(50) DEFAULT NULL,
+                         lost INT(50) DEFAULT NULL
+);
